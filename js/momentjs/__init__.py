14f5c33715aad8f93a3e51ccf60fac30769e0bb1
Jan-Jaap Driessen <devnull@localhost>
2014-11-11 09:29:47 +0100

    Update to 2.8.3. The structure of the moment.js distribution changed, you may need to update your Python imports.

--- js/momentjs/__init__.py
+++ js/momentjs/__init__.py
@@ -2,21 +2,9 @@ from fanstatic import Library, Resource
 
 library = Library('moment.js', 'resources')
 
-moment = Resource(library, 'moment.min.js')
+moment = Resource(library, 'moment.js', minified='moment.min.js')
 
-moment_with_langs = Resource(
+moment_with_locales = Resource(
     library,
-    'moment-with-langs.js',
-    minified='moment-with-langs.min.js')
-
-moment_timezone = Resource(
-    library,
-    'moment-timezone.js',
-    depends=[moment_with_langs],
-    minified='moment-timezone.min.js')
-
-moment_timezone_with_data = Resource(
-    library,
-    'moment-timezone-data.js',
-    depends=[moment_timezone],
-    minified='moment-timezone-data.min.js')
+    'moment-with-locales.js',
+    minified='moment-with-locales.min.js')
